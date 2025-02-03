1be1cc3c041ee7916da6915ad4e6c107cc26bb87
David Tang <687449+iamdtang@users.noreply.github.com>
2025-02-03 14:14:55 -0800

    List invoices

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/invoices', function () {
+    $invoices = DB::table('invoices')->get(); // SELECT * FROM invoices
+    // dd($invoices);
+
+    return view('invoices/index', [
+        'invoices' => $invoices,
+        'invoiceCount' => count($invoices),
+    ]);
+});
