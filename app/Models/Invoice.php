47d7601c806bce807fc65fa8185278c259043687
David Tang <687449+iamdtang@users.noreply.github.com>
2025-02-23 22:12:28 -0800

    Convert the invoice list page to Eloquent

---
+++ app/Models/Invoice.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Invoice extends Model
+{
+    protected $primaryKey = 'InvoiceId';
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class, 'CustomerId');
+    }
+}
