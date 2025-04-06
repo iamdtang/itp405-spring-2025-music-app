e0c5d171975ed8e57de1e8320f695f5c78bfed5b
David Tang <687449+iamdtang@users.noreply.github.com>
2025-04-06 16:54:37 -0700

    Resource routes and route model binding
    php artisan help make:controller
    php artisan make:controller Api/AlbumController --api --model=Album

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\AlbumController;
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::resource('albums', AlbumController::class);
